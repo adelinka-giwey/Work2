39263c63857fb99604e595968aa7a39015e0e012
pc202020 <pc202020@pc202020.ru>
2024-01-23 11:47:14 +0300

    Добавьте файлы проекта.

---
+++ Work2/Page1.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Work2
+{
+    /// <summary>
+    /// Логика взаимодействия для Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void CompliteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(!int.TryParse(NumberATextBox.Text, out int a) || !int.TryParse(NumberBTextBox.Text, out int b) || a < 0 || b < 0)
+            {
+                MessageBox.Show("Введите числа больше 0");
+                return;
+            }
+            else if(a < b)
+            {
+                MessageBox.Show("Число А должно быть больше B");
+            }
+
+            int answer = a / b;
+
+            AnswerTextBox.Text = $"Число отрезков: {answer}";
+        }
+    }
+}
